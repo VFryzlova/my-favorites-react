f87405f014a9299ef49b0cc855b316ed5e78e2b2
VFryzlova <vendulkaf@gmail.com>
2023-01-09 15:02:15 +0100

    header refactoring

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,33 +1,30 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { useQuery } from "react-query";
-import { HeaderEl } from './styles';
+import { useQuery } from 'react-query'
+import { HeaderEl } from './styles'
+
+const getTodaysDate = () => {
+    const date = new Date()
+    return date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear()
+}
+
+const fetchNameDay = async () => {
+    const res = await fetch('https://svatky.adresa.info/json')
+    return res.json()
+}
 
 const Header = () => {
+	const { isLoading, isError, data } = useQuery('nameDay', fetchNameDay)
 
-    const getTodaysDate = () => {
-        const date = new Date()
-        const test = date.getDate() + '.' + (date.getMonth() + 1) + '.' + date.getFullYear()
-        return test
-    }
-
-    // Get today's name day
-    const { isLoading, isError, data } = useQuery("test", async () => {
-        const res = await fetch('https://svatky.adresa.info/json')
-            return res.json();
-    })
-    
-    return (
-    <HeaderEl>
-        <div className="today">
-            <Link to="/login">Login</Link>
-            <h1 className="hi">Hello!</h1>
-            <p className="date">It is {getTodaysDate()}</p>
-            <p className="nameDay">{isLoading || isError ? '' : data[0].name + ' has a name-day'} </p>
-            <input type="search" name="" id="" placeholder="Search.." />    
-        </div>    
-    </HeaderEl>
-    )
+	return (
+		<HeaderEl>
+			<Link to="/login">Login</Link>
+			<h1>Hello!</h1>
+			<p>It is {getTodaysDate()}</p>
+			<p className="name-day">{isLoading || isError ? '' : data[0].name + ' has a name-day'}</p>
+			<input type="search" placeholder="Search.." />
+		</HeaderEl>
+	)
 }
 
 export default Header
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -5,30 +5,31 @@ export const HeaderEl = styled.header`
     background: ${Colors.linear.blue};
     color: ${Colors.white};
     margin-bottom: -32px;
+    padding: 32px 32px 64px;
+    display: flex;
+    flex-direction: column;
+    row-gap: 1rem;
 
-    .today {
-        padding: 32px 32px 64px;
-        display: flex;
-        flex-direction: column;
-        row-gap: 1rem;
+    // Logout link
+    a {
+        color: ${Colors.white};
+    }
 
-        // Logout link
-        a {
-            color: ${Colors.white};
-        }
+    .name-day {
+        height: 25px;
+    }
 
-        // Search
-        input {
-            all: unset;
-            background-color: ${Colors.transparent.white};
-            height: 2.5rem;
-            width: 80%;  
-            border-radius: 2rem;
-            padding: 0 1rem;
+    // Search
+    input {
+        all: unset;
+        background-color: ${Colors.transparent.white};
+        height: 2.5rem;
+        width: 80%;  
+        border-radius: 2rem;
+        padding: 0 1rem;
 
         &::placeholder {
             color: rgba(255,255,255, 0.4);
         }
-    }
-}
-`;
+    }   
+`
