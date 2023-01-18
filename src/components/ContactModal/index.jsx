2749e542ece99f1de6814d8e2069383b1a7d3099
VFryzlova <vendulkaf@gmail.com>
2023-01-18 18:11:54 +0100

    contact modal

--- src/components/ContactModal/index.jsx
+++ src/components/ContactModal/index.jsx
@@ -1,13 +1,20 @@
 import { useQuery } from 'react-query'
+import { useState } from 'react'
 import { Section } from '../Section/styles'
 import { Modal } from './styles'
-import Colors from "../../variables/Colors";
-import Icon from '../Icon';
 import EventTile from '../EventTile';
 import { fetchContactEvents } from '../../fetchers/fetchContactEvents'
 import defaultAvatar from '../.././assets/account_circle_black_48dp.svg'
+import arrowBack from '../.././assets/arrow_back_ios_white_36dp.svg'
+import contactMenu from '../.././assets/more_vert_24dp.svg'
 
 const ContactModal = ({contact, onBack}) => {
+    const [menuModalOpen, setMenuModalOpen] = useState(false)
+
+    const menuModalOpenHandler = () => { 
+        menuModalOpen ? setMenuModalOpen(false) : setMenuModalOpen(true)
+    }
+
     const backHandler = () => { onBack() }
 
     const { isLoading, isError, data: events } = useQuery(['contactEvents', contact.id], fetchContactEvents)
@@ -17,16 +24,31 @@ const ContactModal = ({contact, onBack}) => {
 
     return (
         <Modal>
-            <i className="arrow-back" onClick={backHandler}><Icon type="arrow-back" size={32} color={Colors.grey.dark} /></i>
-            <Section centered>
-                <img src={defaultAvatar} className="avatar" />
-                <h2>{contact.firstName} {contact.lastName}</h2>
-                <p>*21.9.1994, 28 let</p>
-            </Section>
-            <Section centered>
-                <h2>Events</h2>
-                {events.map(event => <EventTile key={event.id} event={event} />)}
-            </Section>
+            <div className="header">
+                <img src={arrowBack} className="arrow-back" onClick={backHandler} />
+                <h3>Detail</h3>
+            </div>
+            <div className="modal-content">
+                <div className="menu-wrapper">
+                    <img src={contactMenu} className="menu-icon" onClick={menuModalOpenHandler} />
+                    {menuModalOpen &&
+                    <div className="menu">
+                        <div>Edit contact</div>
+                        <div>Add event</div>
+                        <div>Delete contact</div>
+                    </div>
+                    }
+                </div>
+                <Section centered>
+                    <img src={defaultAvatar} className="avatar" />
+                    <h2>{contact.firstName} {contact.lastName}</h2>
+                    <p>*21.9.1994, 28 let</p>
+                </Section>
+                <Section centered>
+                    <h2>Events</h2>
+                    {events.map(event => <EventTile key={event.id} event={event} />)}
+                </Section>
+            </div>
         </Modal>
     )
 }
